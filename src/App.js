e34279be337aa10b018ffbf4dd1248b9c7e57506
Basharat Anis <basharatanis@Basharats-MBP.attlocal.net>
2024-01-29 20:05:40 -0600

    build error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/header'
 import Footer from './components/footer';
 import Home from './pages/home'
